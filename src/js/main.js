43c93e186dd97f11628285e2aeb5dd72216858e2
Thomas Sanlis <Thomas.sanlis@gmail.com>
2017-07-13 15:15:54 +0200

    [Menu] Add anchor links

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,22 @@ $(function scrollify() {
         afterRender: function() {}
     });
 });
+/* * * *
+* MENU LINK
+* * * */
+$(".menu__item").click(function(){
+    switch(this.className) {
+        case 'menu__item item__home':
+        $.scrollify.move("#Accueil");
+        break;
+        case 'menu__item item__news':
+        $.scrollify.move("#News");
+        break;
+        case 'menu__item item__recipes':
+        $.scrollify.move("#Recettes");
+        break;
+        case 'menu__item item__contact':
+        $.scrollify.move("#Contact");
+        break;
+    }
+});
